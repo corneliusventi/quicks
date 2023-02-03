957776bc6bad1b41be46f887cba45a352dcabc11
Cornelius Venti <corneliusventi@gmail.com>
2023-02-03 13:54:52 +0700

    basic layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,29 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <main>Quicks</main>;
+  return (
+    <main className="flex h-screen bg-gray-1">
+      <div className="h-full w-[285px] border-r border-gray-6"></div>
+      <div className="h-full w-full">
+        <div className="flex h-[58px] w-full bg-gray-2">
+          <Image
+            className="ml-[26px] mt-[19px] mb-[23px]"
+            width={16}
+            height={16}
+            src="/search.svg"
+            alt="search icon"
+          />
+        </div>
+
+        <button className="absolute bottom-[27px] right-[34px] rounded-full bg-blue-1 p-2">
+          <Image
+            width={56}
+            height={56}
+            src="/cloud-lightning.svg"
+            alt="quick icon"
+          />
+        </button>
+      </div>
+    </main>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,16 @@ module.exports = {
       fontFamily: {
         sans: ["var(--font-lato)", ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        gray: {
+          1: "#333333",
+          2: "#4F4F4F",
+          6: "#F2F2F2",
+        },
+        blue: {
+          1: "#2F80ED",
+        },
+      },
     },
   },
   plugins: [],

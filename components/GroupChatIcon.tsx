f8af2ce6ebfc6536eed0e695c7828af101e1c694
Cornelius Venti <corneliusventi@gmail.com>
2023-02-06 21:56:19 +0700

    fix chat list

--- components/GroupChatIcon.tsx
+++ components/GroupChatIcon.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function GroupChatIcon() {
   return (
-    <div className="relative flex w-12 justify-end">
+    <div className="relative flex w-14 justify-end">
       <div className="absolute right-4 z-0 flex h-8 w-8 items-center justify-center rounded-full bg-gray-5">
         <Image height={16} width={16} src="/person.svg" alt="person 1 icon" />
       </div>
--- components/PersonalChatIcon.tsx
+++ components/PersonalChatIcon.tsx
@@ -6,7 +6,7 @@ type PersonalChatIconProps = {
 
 export default function PersonalChatIcon({ name }: PersonalChatIconProps) {
   return (
-    <div className="flex w-12 justify-center">
+    <div className="flex w-14 justify-center">
       <div className="flex h-8 w-8 items-center justify-center rounded-full bg-blue-1 font-bold text-white">
         {name[0]}
       </div>

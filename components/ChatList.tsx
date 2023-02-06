d40770e6863657ebe1e75d6e2a681ec687df6171
Cornelius Venti <corneliusventi@gmail.com>
2023-02-06 21:35:01 +0700

    add scrollbar to chatlist

--- components/ChatList.tsx
+++ components/ChatList.tsx
@@ -7,8 +7,8 @@ type ChatListProps = {
 
 export default function ChatList({ chats, selectChat }: ChatListProps) {
   return (
-    <div className="divide-y divide-gray-1">
-      {chats.map((chat, index) => (
+    <div className="divide-y divide-gray-1 overflow-y-auto pr-3 scrollbar-thin scrollbar-track-white scrollbar-thumb-gray-4 scrollbar-thumb-rounded-full">
+      {chats.map((chat) => (
         <ChatItem
           chat={chat}
           key={chat.id}
